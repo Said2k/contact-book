ec6fdfe72651ebe25a5d1ae5de018aaa638dac61
said Lansarov <saidlansarov12@gmail.com>
2022-12-12 17:57:00 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AddContact from './components/AddContact';
+import AddContactList from './components/AddContactList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+   const [book, setBook] = useState([])
+   const [bookEdit, setBookEdit] = useState({})
+    function handleContact(obj){
+        let newBook = [...book]
+        newBook.push(obj)
+       setBook(newBook) 
+    
+    }
+
+    function deleteContact(id){
+        let newBook = book.filter((item)=>{
+            return item.id !==id
+        })
+        setBook(newBook)
+    }
+    function handleEdit(index){
+       setBookEdit(book[index])
+    }
+    function handleSave(newBook){
+        let newContact = book.map((item)=>{
+            if(item.id == newBook.id){
+                return  newBook
+            }
+            return item
+        })
+        console.log(newContact);
+        book.push(newBook)
+        book.splice(newBook.id , 2 )
+    }
+    return (
+     
+        <div>
+            <AddContact handleContact={handleContact}/>
+            <AddContactList  book= {book} deleteContact={deleteContact} bookEdit= {bookEdit} setBookEdit ={setBookEdit} handleEdit = {handleEdit} handleSave = {handleSave}/>
+            
+        </div>
+        
+       
+    );
+};
 
 export default App;
